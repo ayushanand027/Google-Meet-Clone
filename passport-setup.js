80130ad8a607c3f9aee251f2ad945818b34e9598
ayush s <ayushs@ayushs-MacBook-Air.local>
2023-07-05 20:46:59 +0530

    fifth

--- passport-setup.js
+++ passport-setup.js
@@ -1,7 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-require('dotenv').config()
-
+require('dotenv').config({ path: './public/.env' });
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
